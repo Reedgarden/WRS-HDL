d4e6f99790b12b817e1d004cc7136af60b6b5e96
Grzegorz Daniluk <grzegorz.daniluk@cern.ch>
2013-07-22 16:07:45 +0200

    wrsw_tatsu: fix manifest

--- modules/wrsw_tatsu/Manifest.py
+++ modules/wrsw_tatsu/Manifest.py
@@ -3,8 +3,4 @@ files = [
   "wrsw_tatsu_pkg.vhd",      
   "tatsu_wbgen2_pkg.vhd",  
   "tatsu_wishbone_controller.vhd",  
-  "../../ip_cores/general-cores/modules/wishbone/wishbone_pkg.vhd",
-  "../../ip_cores/general-cores/modules/common/gencores_pkg.vhd",
-  "../../ip_cores/general-cores/modules/wishbone/wbgen2/wbgen2_pkg.vhd",
-  "../../ip_cores/general-cores/modules/wishbone/wb_slave_adapter/wb_slave_adapter.vhd",
   ]
