4a124e7273091c39e764baf5591d7b356108b31f
Grzegorz Daniluk <grzegorz.daniluk@cern.ch>
2013-09-10 11:07:22 +0200

    [switch-optimization]: use 8-bit registers in wb_simple_pwm
    The values used by WR Switch software fit in 8-bit registers.

--- top/bare_top/scb_top_bare.vhd
+++ top/bare_top/scb_top_bare.vhd
@@ -1010,6 +1010,7 @@ begin
   U_PWM_Controller : xwb_simple_pwm
     generic map (
       g_num_channels        => 2,
+      g_regs_size           => 8,
       g_interface_mode      => PIPELINED,
       g_address_granularity => BYTE)
     port map (
